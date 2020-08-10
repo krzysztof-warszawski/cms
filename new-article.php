76d24a8bbd378ee55e4657a2735392a84ffc8554
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-08-10 13:16:48 +0200

    add edit-article; refactor includes/article.php -> add function validateArticle

--- new-article.php
+++ new-article.php
@@ -1,8 +1,8 @@
 <?php
 
 require 'includes/database.php';
+require 'includes/article.php';
 
-$errors = [];
 $title = '';
 $content = '';
 $published_at = '';
@@ -13,29 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $content = $_POST['content'];
     $published_at = $_POST['published_at'];
 
-    if ($title == '') {
-        $errors[] = 'Title is required';
-    }
-    if ($content == '') {
-        $errors[] = 'Content is required';
-    }
-
-    if ($published_at != '') {
-        $date_time = date_create_from_format('Y-m-d H:i:s', $published_at);
-
-        if ($date_time === false) {
-
-            $errors[] = 'Invalid date and time';
-
-        } else {
-
-            $date_errors = date_get_last_errors();
-
-            if ($date_errors['warning_count'] > 0) {
-                $errors[] = 'Invalid date and time';
-            }
-        }
-    }
+    $errors = validateArticle($title, $content, $published_at);
 
     if (empty($errors)) {
 
@@ -78,33 +56,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h2>New article</h2>
 
-<?php if (! empty($errors)) : ?>
-    <ul>
-        <?php foreach ($errors as $error) : ?>
-            <li><?= $error ?></li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
-
-<form method="post">
-
-    <div>
-        <label for="title">Title</label>
-        <input name="title" id="title" placeholder="Article title" value="<?= htmlspecialchars($title); ?>">
-    </div>
-
-    <div>
-        <label for="content">Content</label>
-        <textarea name="content" rows="4" cols="40" id="content" placeholder="Article content"><?= htmlspecialchars($content); ?></textarea>
-    </div>
-
-    <div>
-        <label for="published_at">Publication date and time</label>
-        <input type="text" name="published_at" id="published_at" value="<?= htmlspecialchars($published_at); ?>">
-    </div>
-
-    <button>Add</button>
-
-</form>
+<?php require 'includes/article-form.php'; ?>
 
 <?php require 'includes/footer.php'; ?>
