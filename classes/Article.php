a355cb032387ea65d0fd76b18c1defc9c5a4d30a
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-08-10 16:55:34 +0200

    refactor new-article.php, to use PDO

--- classes/Article.php
+++ classes/Article.php
@@ -173,6 +173,41 @@ class Article
 
         return $stmt->execute();
     }
+
+    /**
+     * Insert a new article with its current property values
+     *
+     * @param object $conn Connection to the database
+     *
+     * @return boolean True if the insert was successful, false otherwise
+     */
+    public function create($conn) {
+
+        if ($this->validate()) {
+
+            $sql = "INSERT INTO article (title, content, published_at)
+                    VALUES (:title, :content, :published_at)";
+
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+            $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
+
+            if ($this->published_at == '') {
+                $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_NULL);
+            } else {
+                $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_STR);
+            }
+
+            if ($stmt->execute()) {
+                $this->id = $conn->lastInsertId();
+                return true;
+            }
+
+        } else {
+            return false;
+        }
+    }
 }
 
 
