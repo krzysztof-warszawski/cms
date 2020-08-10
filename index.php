e0dbfabca5ed85b5e279a4a9bd17ce7f0f7c3905
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-08-11 00:31:42 +0200

    update index.php, get the page number from the query string

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'includes/init.php';
 
 $conn = require 'includes/db.php';
 
-$paginator = new Paginator(1,4);
+$paginator = new Paginator($_GET['page'] ?? 1,4);
 
 $articles = Article::getPage($conn, $paginator->limit, $paginator->offset);
 
