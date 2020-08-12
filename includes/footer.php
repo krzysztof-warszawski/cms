e418bfa472a0ac697a542e7dae57018d2aa2521d
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-08-12 11:48:45 +0200

    use jquery to validate a form

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
             crossorigin="anonymous"></script>
 
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
 
     <script src="/cms/js/script.js"></script>
 
