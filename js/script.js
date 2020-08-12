2393918d564876d71575568fd21e36ec841c9158
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-08-12 11:19:38 +0200

    use jquery to delete an article or image

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$("a.delete").on("click", function (e) {
+
+    e.preventDefault();
+    if (confirm("Are you sure?")) {
+
+        var frm = $("<form>");
+        frm.attr('method', 'post');
+        frm.attr('action', $(this).attr('href'));
+        frm.appendTo("body");
+        frm.submit();
+    }
+});
